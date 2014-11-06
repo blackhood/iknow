8a8078d8c9e85486cbdf5a1047a86c2b84649e6a
blackhood <cwcaizu@gmail.com>
2014-11-06 16:37:49 -0600

    simon: added front end pages: homepage, sign up sign on page and test.html

---
+++ front_end/sign_up.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<html>
+<?php
+    if (isset($_POST['submit'])) {
+        echo "inside";
+        $name = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+    }
+?>
+<head>
+    <title>Sign Up</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <script src="js/jquery.min.js"></script>
+    <script src="js/skel.min.js"></script>
+    <script src="js/skel-layers.min.js"></script>
+    <script src="js/init.js"></script>
+    <noscript>
+        <link rel="stylesheet" href="css/skel.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/style-xlarge.css" />
+    </noscript>
+</head>
+<body id="top">
+
+<!-- Header -->
+<header id="header" class="skel-layers-fixed">
+    <h1><a href="index.html">Iknow</a></h1>
+</header>
+
+<!-- Main -->
+<section id="main" class="wrapper style1">
+    <header class="major">
+        <h2>Welcome to sign up page</h2>
+        <p>Please enter your information down here to sign up</p>
+    </header>
+    <div class="container">
+        <form id="form" method="post" action="sign_up.php" >
+            <section>
+                <h2>Username</h2>
+                <p> <input type="text" id="user" name="username" placeholder="Username" /> </p>
+                <h2>Password</h2>
+                <p><input type="email" id="email" name="email" placeholder="Password" /></p>
+
+                <h2>Re-enter Password</h2>
+                <p><input type="email" id="email" name="email" placeholder="Confirm Password" /></p>
+                <h2>Email</h2>
+                <p><input type="email" id="email" name="email" placeholder="Email" /></p>
+
+                <ul class="actions">
+                    <li><a href="test.html" class="button big special" name="submit" type="submit">Sign Up</a></li>
+                </ul>
+            </section>
+        </form>
+    </div>
+</section>
+
+<!-- Footer -->
+<footer id="footer">
+    <div class="container">
+        <ul class="copyright">
+            <li>&copy; CS242. All rights reserved.</li>
+        </ul>
+    </div>
+</footer>
+</body>
+</html>
